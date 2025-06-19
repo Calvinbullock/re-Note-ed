abc938cf3a9625f34f0ebd3fc24daf3cbb972fe4
Calvinbullock <calvinnbullock@gmail.com>
2025-06-18 19:26:19 -0700

    to squish

--- src/App.js
+++ src/App.js
@@ -7,24 +7,24 @@ import NotePage from "./components/notePage/NotePage";
 import SignInPage from "./components/signIn-SignUp/SignInPage.tsx";
 import SignUpPage from "./components/signIn-SignUp/SignUpPage.tsx";
 import { AppProvider } from "./components/AppContext.tsx";
-//import TaskPage from "./components/taskPage/TaskPage";
+import TaskPage from "./components/taskPage/TaskPage";
 import Home from "./components/Home";
 
 /*  ===============================================
  *  COMPONENT DEFINITION
  * ============================================= */
 export default function App() {
-  return (
-    <AppProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/NotePage" element={<NotePage />} />
-          {/* <Route path="/TaskPage" element={<TaskPage/>} /> */}
-          <Route path="/SignIn" element={<SignInPage />} />
-          <Route path="/SignUp" element={<SignUpPage />} />
-        </Routes>
-      </BrowserRouter>
-    </AppProvider>
-  );
+    return (
+        <AppProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home/>} />
+                    <Route path="/NotePage" element={<NotePage/>} />
+                    <Route path="/TaskPage" element={<TaskPage/>} />
+                    <Route path="/SignIn" element={<SignInPage />} />
+                    <Route path="/SignUp" element={<SignUpPage />} />
+                </Routes>
+            </BrowserRouter>
+        </AppProvider>
+    );
 }
